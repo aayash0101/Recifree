529ee3e3a1176d6305452acabfcd1c9c40a2904c
Aayash <dangaayash@gmail.com>
2025-12-05 21:49:47 +0530

    backend setup done with auth routes

--- server/app.js
+++ server/app.js
@@ -2,12 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = require('./config/db');
 connectDB();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -15,9 +19,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
