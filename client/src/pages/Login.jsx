dee29d87ab1afd37c58054b19b8ad70024051a02
Aayash <dangaayash@gmail.com>
2025-12-05 23:32:23 +0530

    Frontent login page done

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Login() {
+    const [input, setInputs] = useState({ email : '', password : ''});
+    const [error, setError] = useState('');
+    const { Login } = useAuth();
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value}))
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError('');
+        try {
+            const res = await fetch('auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(inputs)
+            });
+            const data = await res.json();
+            if (!res.ok) throw new Error(data.message || 'Login failed');
+            Login(data.user, data.token);
+            navigate('/');
+        } catch (err){
+            setError(err.message);
+        }
+    };
+    return (
+        <>
+        <Navbar />
+        <div className = "form-container">
+            <h2>Login</h2>
+            <form onSubmit = {handleSubmit} className="auth-form">
+                <input type = "email" name="email" placeholder="Email" value={inputs.email} onChange={handleChange} required />
+                <input type = "password" name="password" placeholder="Password" value={inputs.password} onChange={handleChange} required />
+                <button type="submit"></button>
+            </form>
+            {error && <div className = "error-msg">{error}</div>}
+            <div>Dont have an account?<Link to='/signup'>Sign Up</Link></div>
+        </div>
+        </>
+    );
+}
