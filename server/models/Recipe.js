5681959e7550eb4c5bf58bbd79b7a92e6ada4202
Aayash <dangaayash@gmail.com>
2025-12-06 16:34:30 +0530

    backend models recipe

---
+++ server/models/Recipe.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const RecipeSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    ingredients : [{
+        type : String,
+    }],
+    instructions : [{
+        type : String,
+    }],
+    category : {
+        type : String,
+        required : true
+    },
+    image : {
+        type : String,
+        required : false
+    },
+    cookingTime : {
+        type : String,
+        required : true
+    },
+});
+
+module.exports = mongoose.model('Recipe', RecipeSchema)
