d17e6c7e9cee0ac5256e5174f9ecb8a75be7e8fd
Aayash <dangaayash@gmail.com>
2025-12-06 18:15:25 +0530

    Navbar done

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+export default function NavBar() {
+    const { user, logout } = useAuth();
+    return (
+        <nav className='navbar'>
+            <div className='nav-logo'>
+                <Link to="/">Recifree</Link>
+            </div>
+            <div className='nav-links'>
+                <Link to="/">Home</Link>
+                <Link to="/recipes">Recipes</Link>
+                <Link to="/favorites">Favorites</Link>
+                <Link to="/ingredients">Shopping List</Link>
+                {user ? (
+                    <>
+                        <Link to="/profile">Profile</Link>
+                        <button className='nav-btn' onClick={logout}>Logout</button>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Sign Up</Link>
+                    </>
+                )}
+            </div>
+        </nav>
+    );
+}
