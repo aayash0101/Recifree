0f9afcaddf5bbcb02644e0c5d71f3df7a7a49ed3
Aayash <dangaayash@gmail.com>
2025-12-10 19:03:06 +0530

    Routers set

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAllRecipes, getRecipe, seedRecipes, createRecipe } = require('../controllers/recipeController');
+const { getAllRecipes, getRecipe, seedRecipes, createRecipe, deleteRecipe, getUserRecipes } = require('../controllers/recipeController');
 
 router.get('/', getAllRecipes);
 router.get('/seed', seedRecipes);
 router.get('/:id', getRecipe);
 router.post('/', createRecipe);
+router.delete('/:id', deleteRecipe);
+router.get('/user/:userId', getUserRecipes);
 
 module.exports = router;
